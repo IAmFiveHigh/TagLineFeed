b51bd2e7ffc58d3035afff4c44b1b9b4eaa436e7
IAmFiveHigh <745170149@qq.com>
2017-05-15 15:40:14 +0800

    添加了点击事件 微修改界面

--- 标签自动换行/LHCTagsView.m
+++ 标签自动换行/LHCTagsView.m
@@ -32,9 +32,9 @@
     for (int i = 0; i<self.tags.count; i++) {
         UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
         button.tag = 300+i;
-        button.backgroundColor = [UIColor lightGrayColor];
+        button.backgroundColor = [UIColor colorWithRed:250.0 / 255.0 green:250.0 / 255.0 blue:250.0 / 255.0 alpha:1];
         button.layer.masksToBounds = YES;
-        button.layer.cornerRadius = 10;
+        button.layer.cornerRadius = 3;
         
         NSString *title = self.tags[i];
         CGSize titleSize = [title boundingRectWithSize:CGSizeMake(self.bounds.size.width, CGFLOAT_MAX) options:NSStringDrawingUsesFontLeading|NSStringDrawingTruncatesLastVisibleLine|NSStringDrawingUsesDeviceMetrics attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:15]} context:nil].size;
@@ -52,10 +52,15 @@
         }
         
         [button setTitle:title forState:UIControlStateNormal];
-        button.titleLabel.font = [UIFont systemFontOfSize:13];
-        [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        button.titleLabel.font = [UIFont systemFontOfSize:17];
+        [button setTitleColor:[UIColor colorWithRed:153.0 / 255.0 green:153.0 / 255.0 blue:153.0 / 255.0 alpha:1] forState:UIControlStateNormal];
         [self addSubview:button];
+        [button addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchUpInside];
+        
+        
     }
+    
+    
     
     CGRect frame = self.frame;
     frame.size.height = row * 50 + 10 + 30;
@@ -63,12 +68,18 @@
     
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)buttonClick:(UIButton *)sender {
+    if ([self.delegate respondsToSelector:@selector(tagBeClickWithTag:)]) {
+        [self.delegate tagBeClickWithTag:sender.tag - 300];
+    }
 }
-*/
+
+/*
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect {
+ // Drawing code
+ }
+ */
 
 @end
